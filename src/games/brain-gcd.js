23abef27ee32510282021d4fe56741766b99f951
anton urazgildin <antoha91@mail.ru>
2023-06-07 17:33:01 +0300

    rewrite brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,47 +1,23 @@
 import gamesEngine from '../index.js';
 import randomNumberGenerator from '../utils.js';
 
-const ifSimple = (number) => {
-  if (number < 2) {
-    return false;
-  }
-  for (let i = 2; i <= number / 2; i += 1) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const findDevider = (number1, number2) => {
-  const max = Math.max(number1, number2);
-  const min = Math.min(number1, number2);
-  let result;
-  if (min === 0) {
-    result = max;
-    return result;
-  }
-  if (max % min === 0) {
-    result = min;
-    return result;
-  }
-  if (ifSimple(max) === true || ifSimple(min) === true) {
-    result = 1;
-    return result;
-  }
-  for (let i = Math.floor(min / 2); i >= 1; i -= 1) {
-    if ((min % i === 0) && (max % i === 0)) {
-      result = i;
-      break;
-    }
-  }
-  return result;
-};
-
 const expressionRightAnswer = () => {
   const arr = [randomNumberGenerator(), randomNumberGenerator()];
   const expression = `${arr[0]} ${arr[1]}`;
-  const rightAnswer = findDevider(arr[0], arr[1]).toString();
+  const sortedArr = arr.sort((a, b) => a - b);
+  const min = sortedArr[0];
+  const max = sortedArr[1];
+  let rightAnswer;
+  if (min === 0) {
+    rightAnswer = max.toString();
+    return [expression, rightAnswer];
+  }
+  while (sortedArr[1] % sortedArr[0] !== 0) {
+    const remainder = sortedArr[1] % sortedArr[0];
+    sortedArr[1] = sortedArr[0];
+    sortedArr[0] = remainder;
+  }
+  rightAnswer = sortedArr[0].toString();
   return [expression, rightAnswer];
 };
 
