001229cf4cb2b24ef66df519033b66b75e4528a0
anton urazgildin <antoha91@mail.ru>
2023-06-30 11:12:47 +0300

    refactoring according notes (pack4) fixed

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import runbBrainProgression from '../src/games/brain-progression.js';
+import runBrainProgression from '../src/games/brain-progression.js';
 
 runBrainProgression();
